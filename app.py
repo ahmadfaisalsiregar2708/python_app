3f8fdf190f5adbf2bba3e61c8f77c613cb7ac792
ahmadfaisalsiregar2708 <ahmadfaisalsiregar2708@gmail.com>
2022-06-11 12:13:35 +0700

    Update Prediksi 12

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import plotly_express as px
 import pandas as pd
 import numpy as np
-import mysql.connector
+# import mysql.connector
 import statistics
 from sklearn.preprocessing import StandardScaler
 from sklearn import metrics
